6b8c1100c807df920d502ff79f5fa43f3365ed3b
neeldexter <neelamdas83@gmail.com>
2020-10-15 21:36:27 +0530

    Updates

---
+++ precache-manifest.ca4fdeb44b8ec00941f3be69b8abbc4c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5bbd68417b6073fcc4df259e613bc8",
+    "url": "/techVariableAssignment/index.html"
+  },
+  {
+    "revision": "c74586e6a3f732daebc7",
+    "url": "/techVariableAssignment/static/css/2.fb6c816b.chunk.css"
+  },
+  {
+    "revision": "b449c42e2d320cf15496",
+    "url": "/techVariableAssignment/static/css/main.f4252263.chunk.css"
+  },
+  {
+    "revision": "c74586e6a3f732daebc7",
+    "url": "/techVariableAssignment/static/js/2.2b23b9fe.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/techVariableAssignment/static/js/2.2b23b9fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b449c42e2d320cf15496",
+    "url": "/techVariableAssignment/static/js/main.229ad85c.chunk.js"
+  },
+  {
+    "revision": "c67d684fbe77a3d09d67",
+    "url": "/techVariableAssignment/static/js/runtime-main.35ab8fb3.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/techVariableAssignment/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/techVariableAssignment/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/techVariableAssignment/static/media/revicons.2feb69cc.eot"
+  }
+]);
